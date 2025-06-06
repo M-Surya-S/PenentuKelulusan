b7d959c389f8764e6428f309ef2ed7a0f25db943
M-Surya-S <muhammadsurya216@gmail.com>
2025-06-07 07:19:08 +0800

    Update : Perubahan Item Card Pada Dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Alternatif;
 use App\Models\KriteriaBobot;
 use App\Models\SubKriteria;
 use Illuminate\Http\Request;
@@ -10,9 +11,11 @@ class Dashboard extends Controller
 {
     public function index()
     {
-        $kriteria = KriteriaBobot::count();
-        $sub_kriteria = SubKriteria::count();
+        $kriteria_bobot = KriteriaBobot::all();
+        $jumlah_kriteria = $kriteria_bobot->count();
+        $jumlah_sub_kriteria = SubKriteria::count();
+        $alternatif = Alternatif::count();
 
-        return view('dashboard', compact('kriteria', 'sub_kriteria'));
+        return view('dashboard', compact('kriteria_bobot', 'jumlah_kriteria', 'jumlah_sub_kriteria', 'alternatif'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -52,7 +52,7 @@
                                     <div class="numbers">
                                         <p class="text-sm mb-2 text-uppercase font-weight-bold">Jumlah Alternatif</p>
                                         <h5 class="font-weight-bolder mb-0">
-                                            50
+                                            {{ $alternatif }}
                                         </h5>
                                     </div>
                                 </div>
@@ -74,7 +74,7 @@
                                     <div class="numbers">
                                         <p class="text-sm mb-2 text-uppercase font-weight-bold">Jumlah Kriteria</p>
                                         <h5 class="font-weight-bolder mb-0">
-                                            {{ $kriteria }}
+                                            {{ $jumlah_kriteria }}
                                         </h5>
                                     </div>
                                 </div>
@@ -96,7 +96,7 @@
                                     <div class="numbers">
                                         <p class="text-sm mb-2 text-uppercase font-weight-bold">Jumlah Sub Kriteria</p>
                                         <h5 class="font-weight-bolder mb-0">
-                                            {{ $sub_kriteria }}
+                                            {{ $jumlah_sub_kriteria }}
                                         </h5>
                                     </div>
                                 </div>
@@ -125,124 +125,19 @@
                                     <tr>
                                         <td class="w-30">
                                             <div class="d-flex px-2 py-1 align-items-center">
-                                                <div>
-                                                    <img src="../assets/img/icons/flags/US.png" alt="Country flag">
+                                                <div class="ms-3">
+                                                    <h6 class="text-sm mb-0">1.</h6>
                                                 </div>
-                                                <div class="ms-4">
-                                                    <p class="text-xs font-weight-bold mb-0">Country:</p>
-                                                    <h6 class="text-sm mb-0">United States</h6>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                                                <h6 class="text-sm mb-0">2500</h6>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Value:</p>
-                                                <h6 class="text-sm mb-0">$230,900</h6>
-                                            </div>
-                                        </td>
-                                        <td class="align-middle text-sm">
-                                            <div class="col text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                                                <h6 class="text-sm mb-0">29.9%</h6>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="w-30">
-                                            <div class="d-flex px-2 py-1 align-items-center">
-                                                <div>
-                                                    <img src="../assets/img/icons/flags/DE.png" alt="Country flag">
-                                                </div>
-                                                <div class="ms-4">
-                                                    <p class="text-xs font-weight-bold mb-0">Country:</p>
-                                                    <h6 class="text-sm mb-0">Germany</h6>
+                                                <div class="ms-5">
+                                                    <p class="text-xs font-weight-bold mb-0">Nama Peserta:</p>
+                                                    <h6 class="text-sm mb-0">Surya</h6>
                                                 </div>
                                             </div>
                                         </td>
                                         <td>
                                             <div class="text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                                                <h6 class="text-sm mb-0">3.900</h6>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Value:</p>
-                                                <h6 class="text-sm mb-0">$440,000</h6>
-                                            </div>
-                                        </td>
-                                        <td class="align-middle text-sm">
-                                            <div class="col text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                                                <h6 class="text-sm mb-0">40.22%</h6>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="w-30">
-                                            <div class="d-flex px-2 py-1 align-items-center">
-                                                <div>
-                                                    <img src="../assets/img/icons/flags/GB.png" alt="Country flag">
-                                                </div>
-                                                <div class="ms-4">
-                                                    <p class="text-xs font-weight-bold mb-0">Country:</p>
-                                                    <h6 class="text-sm mb-0">Great Britain</h6>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                                                <h6 class="text-sm mb-0">1.400</h6>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Value:</p>
-                                                <h6 class="text-sm mb-0">$190,700</h6>
-                                            </div>
-                                        </td>
-                                        <td class="align-middle text-sm">
-                                            <div class="col text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                                                <h6 class="text-sm mb-0">23.44%</h6>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="w-30">
-                                            <div class="d-flex px-2 py-1 align-items-center">
-                                                <div>
-                                                    <img src="../assets/img/icons/flags/BR.png" alt="Country flag">
-                                                </div>
-                                                <div class="ms-4">
-                                                    <p class="text-xs font-weight-bold mb-0">Country:</p>
-                                                    <h6 class="text-sm mb-0">Brasil</h6>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                                                <h6 class="text-sm mb-0">562</h6>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Value:</p>
-                                                <h6 class="text-sm mb-0">$143,960</h6>
-                                            </div>
-                                        </td>
-                                        <td class="align-middle text-sm">
-                                            <div class="col text-center">
-                                                <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                                                <h6 class="text-sm mb-0">32.14%</h6>
+                                                <p class="text-xs font-weight-bold mb-0">Skor:</p>
+                                                <h6 class="text-sm mb-0">0.55</h6>
                                             </div>
                                         </td>
                                     </tr>
@@ -258,76 +153,22 @@
                         </div>
                         <div class="card-body p-3">
                             <ul class="list-group">
-                                <li
-                                    class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                    <div class="d-flex align-items-center">
-                                        <div class="icon icon-shape icon-sm me-3 bg-gradient-dark shadow text-center">
-                                            <i class="ni ni-mobile-button text-white opacity-10"></i>
+                                @foreach ($kriteria_bobot as $kriteria) 
+                                    @php
+                                        $sub_kriteria = App\Models\SubKriteria::where('id_kriteria', $kriteria->id)->count();
+                                    @endphp
+                                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                                        <div class="d-flex align-items-center">
+                                            <div class="icon icon-shape icon-sm me-3 bg-gradient-dark shadow text-center">
+                                                <i class="ni ni-archive-2 text-white opacity-10"></i>
+                                            </div>
+                                            <div class="d-flex flex-column">
+                                                <h6 class="mb-1 text-dark text-sm">{{ $kriteria->kriteria }}</h6>
+                                                <span class="text-xs">{{ $sub_kriteria }} Sub Kriteria</span>
+                                            </div>
                                         </div>
-                                        <div class="d-flex flex-column">
-                                            <h6 class="mb-1 text-dark text-sm">Devices</h6>
-                                            <span class="text-xs">250 in stock, <span class="font-weight-bold">346+
-                                                    sold</span></span>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex">
-                                        <button
-                                            class="btn btn-link btn-icon-only btn-rounded btn-sm text-dark icon-move-right my-auto"><i
-                                                class="ni ni-bold-right" aria-hidden="true"></i></button>
-                                    </div>
-                                </li>
-                                <li
-                                    class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                    <div class="d-flex align-items-center">
-                                        <div class="icon icon-shape icon-sm me-3 bg-gradient-dark shadow text-center">
-                                            <i class="ni ni-tag text-white opacity-10"></i>
-                                        </div>
-                                        <div class="d-flex flex-column">
-                                            <h6 class="mb-1 text-dark text-sm">Tickets</h6>
-                                            <span class="text-xs">123 closed, <span class="font-weight-bold">15
-                                                    open</span></span>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex">
-                                        <button
-                                            class="btn btn-link btn-icon-only btn-rounded btn-sm text-dark icon-move-right my-auto"><i
-                                                class="ni ni-bold-right" aria-hidden="true"></i></button>
-                                    </div>
-                                </li>
-                                <li
-                                    class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                    <div class="d-flex align-items-center">
-                                        <div class="icon icon-shape icon-sm me-3 bg-gradient-dark shadow text-center">
-                                            <i class="ni ni-box-2 text-white opacity-10"></i>
-                                        </div>
-                                        <div class="d-flex flex-column">
-                                            <h6 class="mb-1 text-dark text-sm">Error logs</h6>
-                                            <span class="text-xs">1 is active, <span class="font-weight-bold">40
-                                                    closed</span></span>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex">
-                                        <button
-                                            class="btn btn-link btn-icon-only btn-rounded btn-sm text-dark icon-move-right my-auto"><i
-                                                class="ni ni-bold-right" aria-hidden="true"></i></button>
-                                    </div>
-                                </li>
-                                <li class="list-group-item border-0 d-flex justify-content-between ps-0 border-radius-lg">
-                                    <div class="d-flex align-items-center">
-                                        <div class="icon icon-shape icon-sm me-3 bg-gradient-dark shadow text-center">
-                                            <i class="ni ni-satisfied text-white opacity-10"></i>
-                                        </div>
-                                        <div class="d-flex flex-column">
-                                            <h6 class="mb-1 text-dark text-sm">Happy users</h6>
-                                            <span class="text-xs font-weight-bold">+ 430</span>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex">
-                                        <button
-                                            class="btn btn-link btn-icon-only btn-rounded btn-sm text-dark icon-move-right my-auto"><i
-                                                class="ni ni-bold-right" aria-hidden="true"></i></button>
-                                    </div>
-                                </li>
+                                    </li>
+                                @endforeach
                             </ul>
                         </div>
                     </div>
